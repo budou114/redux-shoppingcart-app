c4160d0af08256ee334941360b4a22eb3379b0e2
budou114 <58349030+budou114@users.noreply.github.com>
2022-10-23 13:10:27 +0900

    22. ナビゲーションバーを作成してみよう

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>Redux Shopping</h3>
+        <div className="nav-container">
+          ロゴ
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
+          </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
